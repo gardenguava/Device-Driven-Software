377e1754b4cbe42d7348244f887ccaff2deee914
buildtest <gardenguava@users.noreply.github.com>
2015-09-18 14:50:33 -0400

    Update DeviceClass.java

--- Device-Driven-Software/src/deviceDrivenSoftware/DeviceClass.java
+++ Device-Driven-Software/src/deviceDrivenSoftware/DeviceClass.java
@@ -2,7 +2,7 @@ package deviceDrivenSoftware;
 
 public class DeviceClass {
 	public static void main(String args[]) {
-		System.out.println("This is the device driven class");
+		System.out.println("This is the device driven class One");
 	}
 
 }
